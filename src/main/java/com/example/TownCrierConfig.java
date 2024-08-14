e185177775bfa304afbb00b65a30ecde53eb4ce8
Justin Weaver <weaverjustin01@gmail.com>
2024-08-14 19:39:08 +0100

    First Commit
    Uploading the files

---
+++ src/main/java/com/example/TownCrierConfig.java
@@ -0,0 +1,39 @@
+package net.runelite.client.plugins.towncrier;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("towncrier")
+public interface TownCrierConfig extends Config
+{
+	@ConfigItem(
+		keyName = "cry1",
+		name = "The First Cry",
+		description = "One of the town crier messages"
+	)
+	default String cry1()
+	{
+		return "Have fun playing RuneScape!";
+	}
+
+	@ConfigItem(
+			keyName = "cry2",
+			name = "The Second Cry",
+			description = "One of the town crier messages"
+	)
+	default String cry2()
+	{
+		return "";
+	}
+
+	@ConfigItem(
+			keyName = "cry3",
+			name = "The Third Cry",
+			description = "One of the town crier messages"
+	)
+	default String cry3()
+	{
+		return "";
+	}
+}
---
+++ src/main/java/com/example/TownCrierPlugin.java
@@ -0,0 +1,67 @@
+package net.runelite.client.plugins.towncrier;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.ThreadLocalRandom;
+import net.runelite.api.Actor;
+import java.util.ArrayList;
+import net.runelite.api.Client;
+import net.runelite.api.events.OverheadTextChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Town Crier's Cries"
+)
+public class TownCrierPlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private TownCrierConfig config;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		log.info("Town Crier's Cries started!");
+	}
+
+	@Subscribe
+	public void onOverheadTextChanged(OverheadTextChanged event)
+	{
+		Actor actor = event.getActor();
+		if (actor.getName() != null && actor.getName().equals("Town Crier")){
+			String message = getMessage();
+			actor.setOverheadText(message);
+		}
+	}
+
+	public String getMessage(){
+		ArrayList<String> messages = new ArrayList<String>();
+		if(!config.cry1().isEmpty())
+		{
+			messages.add(config.cry1());
+		}
+		if(!config.cry2().isEmpty())
+		{
+			messages.add(config.cry2());
+		}
+		if(!config.cry3().isEmpty())
+		{
+			messages.add(config.cry3());
+		}
+		int rand = ThreadLocalRandom.current().nextInt(0, messages.size());
+		return messages.get(rand);
+	}
+
+	@Provides
+	TownCrierConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(TownCrierConfig.class);
+	}
+}
